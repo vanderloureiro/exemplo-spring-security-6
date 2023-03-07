b63ba3feeaa403f16e2ad2ca4266e66e0c9d914a
Vanderlei Loureiro <vanderlei.loureiro@evoluum.com.br>
2023-03-07 14:47:59 -0300

    remocao de lombok

--- src/main/java/dev/vanderloureiro/exemplosecurity/core/CustomBasicAuthFilter.java
+++ src/main/java/dev/vanderloureiro/exemplosecurity/core/CustomBasicAuthFilter.java
@@ -4,7 +4,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -14,30 +13,30 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.Base64;
 
-@Slf4j
 @Service
 public class CustomBasicAuthFilter extends OncePerRequestFilter {
 
-    private static final int BEARER_LENGTH = 6;
-    private static final String EXAMPLE_USERNAME = "usuario";
-    private static final String EXAMPLE_PASSWORD = "senha";
+    private static final int BASIC_LENGTH = 6;
+    private static final String EXEMPLO_USERNAME = "usuario";
+    private static final String EXEMPLO_PASSWORD = "senha";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
         var headerAuthorization = request.getHeader("Authorization");
-        log.info(headerAuthorization);
         if (headerAuthorization == null || !headerAuthorization.startsWith("Basic ")) {
             filterChain.doFilter(request, response);
             return;
         }
-        var basicToken = headerAuthorization.substring(BEARER_LENGTH);
+        var basicToken = headerAuthorization.substring(BASIC_LENGTH);
         byte[] basicTokenDecoded = Base64.getDecoder().decode(basicToken);
         String basicTokenValue = new String(basicTokenDecoded);
-        basicTokenValue.split(":");
-        if (SecurityContextHolder.getContext().getAuthentication() == null) {
-            var authToken = new UsernamePasswordAuthenticationToken(null, null, null);
+        String[] basicAuthsSplit = basicTokenValue.split(":");
+
+        if (basicAuthsSplit[0].equals(EXEMPLO_USERNAME) && basicAuthsSplit[1].equals(EXEMPLO_PASSWORD)) {
+
+            var authToken = new UsernamePasswordAuthenticationToken(basicAuthsSplit[0], null, null);
             authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authToken);
         }
