65c25e0f8d81f2d428f21ec30648529e5c72aef5
Vanderlei Loureiro <vanderlei.loureiro@evoluum.com.br>
2023-03-07 11:35:12 -0300

    inicio de projeto e consiguracoes de security

---
+++ src/main/java/dev/vanderloureiro/exemplosecurity/core/SecurityConfig.java
@@ -0,0 +1,34 @@
+package dev.vanderloureiro.exemplosecurity.core;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain filterChain(
+            HttpSecurity http,
+            CustomBasicAuthFilter customBasicAuthFilter) throws Exception {
+
+        http
+                .authorizeHttpRequests(
+                        authorizeConfig -> {
+                            authorizeConfig.requestMatchers("/publica").permitAll();
+                            authorizeConfig.anyRequest().authenticated();
+                        }
+                )
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(customBasicAuthFilter)
+                .csrf().disable();
+
+        return http.build();
+    }
+
+}
